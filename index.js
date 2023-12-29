f2fcc2108d7882867ceb6eef5b09c18babc2a472
MarwaneQ <merouane12345bensadallah@gmail.com>
2023-12-29 06:02:22 -0800

    input function

--- index.js
+++ index.js
@@ -2,3 +2,30 @@ let gameName = "Guess The Word";
 document.title= gameName;
 document.querySelector("h1").innerHTML = gameName;
 document.querySelector("footer").innerHTML = `${gameName} Created By Marwane Dev`;
+let numberOfTries = 6;
+let numberOfLetters = 6;
+let currentTry = 1;
+function generateInput() {
+    const inputsContainer = document.querySelector(".inputs")
+
+    for (let i = 1; i <= numberOfTries; i++) {
+        const tryDiv = document.createElement("div");
+        tryDiv.classList.add(`try-${i}`);
+        tryDiv.innerHTML =`<span>Try ${i}</span>`;
+
+        if(i !==1) tryDiv.classList.add('disabled-input')
+
+        for (let j = 1; j <= numberOfLetters ; j++){
+            const input = document.createElement("input")
+            input.type = "text"
+            input.id = `guess-${i}-letter-${j}`
+            input.setAttribute("maxlength","1")
+            tryDiv.appendChild(input)
+        }
+        inputsContainer.appendChild(tryDiv)
+    }
+    inputsContainer.children[0].children[1].focus();
+}
+window.onload =function(){
+    generateInput()
+}
